9a0fd07008920d1e8b1d538390ac2d38c7321ff4
tamir71 <tamir71@gmail.com>
2021-02-21 22:55:16 -0800

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,3 @@ def subprocess_cmd(command):
     print proc_stdout
 
 subprocess_cmd('docker start nervous_curran; docker exec -it nervous_curran bash -c "bash ~/test.sh"; docker stop nervous_curran')
-
-client = docker.DockerClient(base_url='unix:///var/run/docker.sock')
-for i in client.containers.list():
-    print(i.stats(stream=False))
